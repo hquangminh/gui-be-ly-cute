23bd65a1bfbe76dbe35ba4b99f04691b46b8e7d8
charleshuynh <66006968+hquangminh@users.noreply.github.com>
2023-09-03 10:22:37 +0700

    Update config.js
    check commit conflig.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Bé Tuyết ƠI!',
+    introTitle: 'Bé Ly Ly ƠI!',
     introDesc: `Nhấn Vào Dưới Bé Minh Có Điều Muốn Nói`,
     btnIntro: 'Nhấn Đây Nè',
     title: 'Cho Bé Minh Xin Lỗi Nhen 😙',
@@ -8,7 +8,7 @@ const CONFIG = {
     btnNo: 'Không, Biến Đi :3',
     question:'Giờ Nhắn Chửi Gì Bé Minh Nghe Đây <3',
     btnReply: 'Gửi Đó Bớt Láo Lại Nghe Chưa <3',
-    reply: 'Bé Tuyết Yêu Bé Minh Nhiều <33333333',
+    reply: 'Bé Ly Yêu Bé Minh Nhiều <33333333',
     mess: 'Biết rồi 🥰. Thương bé nhiều nhiều 😘😘',
     messDesc: 'Cuối Tuần anh dẫn bé đi chơi nha công chúa của tui.',
     btnAccept: 'Okiiiii lun <3',
